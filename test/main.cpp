0b3cc8f8addeadc508f04bad3c202472668c46c8
shaohedu <dushaohe_hit@163.com>
2021-06-29 22:36:04 +0800

    add to string

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 #include "math.hpp"
+#include "to_string_test.hpp"
 
 int main(int argc, const char* argv[]) {
     for (int i = 1; i < argc; i++) {
         std::cout << argv[i] << "\t" << Square(std::stoi(argv[i])) << std::endl;
     }
+    toStringTest();
     
     return 0;
 }
---
+++ test/to_string_test.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void toStringTest()
+{
+    int i;
+    cin>>i;
+    string  palindrom = to_string (i);
+    cout<<palindrom<<" testl"<<endl;
+}
