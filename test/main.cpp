07dd17c0076534ce8675983e137420da7ff61ba9
shaohedu <dushaohe_hit@163.com>
2021-06-30 21:58:34 +0800

    in line with google style

--- test/main.cpp
+++ test/main.cpp
@@ -2,12 +2,15 @@
 
 #include "math.hpp"
 #include "to_string_test.hpp"
+#include "is_palindrome.hpp"
 
 int main(int argc, const char* argv[]) {
     for (int i = 1; i < argc; i++) {
         std::cout << argv[i] << "\t" << Square(std::stoi(argv[i])) << std::endl;
     }
-    toStringTest();
-    
+    //toStringTest();
+    int x;
+    std::cin >> x ;
+    std::cout << isPalindrome(x) << std::endl;
     return 0;
 }
--- test/to_string_test.hpp
+++ test/to_string_test.hpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 using namespace std;
-void toStringTest()
-{
+
+void toStringTest() {
     int i;
-    cin>>i;
-    string  palindrom = to_string (i);
-    cout<<palindrom<<" testl"<<endl;
+    cin >> i;
+    string palindrom = to_string(i);
+    cout << palindrom << " testl" << endl;
 }
